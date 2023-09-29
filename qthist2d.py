26eed25e415a36aea6a1c5ed7367983e27a6b1a7
James Davenport <jradavenport@gmail.com>
2023-09-28 17:02:37 -0700

    added binned_statistic_qt method and example

--- qthist2d.py
+++ qthist2d.py
@@ -95,10 +95,113 @@ def qthist(x, y, N=5, thresh=4, rng=[], density=True):
     return num, xmin, xmax, ymin, ymax
 
 
+def binned_statistic_qt(x, y, v=None, statistic='density', edges=None):
+    '''
+    Computed statistics inlike scipy's binned_statistic_2d, but for QT's, and assuming np.nan-friendly stats
+    
+    Parameters
+    ----------
+    x, y : the 2 arrays of new data to compute the histogram from
+    v : array of values to compute statistic over. Needs to match 
+        x and y
+    edges : x and y edges of the Quad Tree, expanded as
+        xmin,xmax,ymin,ymax = edges
+    statistic : string or callable, optional
+        The statistic to compute (default value is 'density')
+        
+        The following statistics are available:
+            * 'density' : compute the count of points within each bin, then
+                divide by the area of the bin in (x,y) space. This is useful
+                for making a histogram-like plot from the quad tree.
+            * 'mean' : compute the mean of values for points within each bin.
+                Empty bins will be represented by NaN. 
+            * 'median' : compute the median of values for points within each
+                bin. Empty bins will be represented by NaN. 
+            * 'count' : compute the count of points within each bin.  This is
+                identical to an unweighted histogram.  `v` array is not
+                referenced. 
+            * 'sum' : compute the sum of values for points within each bin. 
+            * 'std' : compute the standard deviation within each bin.
+                Empty bins will be represented by NaN.
+            * 'min' : compute the minimum of values for points within each bin.
+                Empty bins will be represented by NaN.
+            * 'max' : compute the maximum of values for point within each bin.
+                Empty bins will be represented by NaN.
+            * function : a user-defined function which takes a 1D array of
+                values, and outputs a single numerical statistic. This function
+                will be called on the values in each bin. 
+
+    Returns
+    -------
+    statistic : computed statistic within each bin
+    '''
+    
+    #edges must be defined from QT... or we need to run QT to get them
+    xmin,xmax,ymin,ymax = edges
+    
+    # check that statistic is callable
+    known_stats = ['mean', 'median', 'count', 'sum', 'std', 'min', 'max', 'density']
+    if not callable(statistic) and statistic not in known_stats:
+        raise ValueError(f'invalid statistic {statistic!r}')
+   
+    result = np.zeros_like(xmin, dtype=np.float64)
+
+    if statistic == 'density':
+        for k in range(len(xmin)):
+            result[k] = np.sum((x >= xmin[k]) & (x < xmax[k]) & (y >= ymin[k]) & (y < ymax[k]))
+        result = result / ((ymax - ymin) * (xmax - xmin)) / result.sum()
+        
+    elif statistic == 'count':
+        for k in range(len(xmin)):
+            result[k] = np.sum((x >= xmin[k]) & (x < xmax[k]) & (y >= ymin[k]) & (y < ymax[k]))
+            
+    elif statistic == 'sum':
+        for k in range(len(xmin)):
+            b = (x >= xmin[k]) & (x < xmax[k]) & (y >= ymin[k]) & (y < ymax[k])
+            result[k] = np.nansum(v[b])
+            
+    elif statistic in {'mean', np.mean, np.nanmean}:
+        result.fill(np.nan)
+        for k in range(len(xmin)):
+            b = (x >= xmin[k]) & (x < xmax[k]) & (y >= ymin[k]) & (y < ymax[k])
+            result[k] = np.nanmean(v[b])
+            
+    elif statistic in {'median', np.median, np.nanmedian}:
+        result.fill(np.nan)
+        for k in range(len(xmin)):
+            b = (x >= xmin[k]) & (x < xmax[k]) & (y >= ymin[k]) & (y < ymax[k])
+            result[k] = np.nanmedian(v[b])
+    elif statistic in {'min', np.min, np.nanmin}:
+        result.fill(np.nan)
+        for k in range(len(xmin)):
+            b = (x >= xmin[k]) & (x < xmax[k]) & (y >= ymin[k]) & (y < ymax[k])
+            result[k] = np.nanmin(v[b])
+    elif statistic in {'max', np.max, np.nanmax}:
+        result.fill(np.nan)
+        for k in range(len(xmin)):
+            b = (x >= xmin[k]) & (x < xmax[k]) & (y >= ymin[k]) & (y < ymax[k])
+            result[k] = np.nanmax(v[b])
+    elif statistic in {'std', np.std, np.nanstd}:
+        result.fill(np.nan)
+        for k in range(len(xmin)):
+            b = (x >= xmin[k]) & (x < xmax[k]) & (y >= ymin[k]) & (y < ymax[k])
+            result[k] = np.nanstd(v[b])
+
+    # need to test callable statistic returns something that fits in result
+    elif callable(statistic):
+        for k in range(len(xmin)):
+            b = (x >= xmin[k]) & (x < xmax[k]) & (y >= ymin[k]) & (y < ymax[k])
+            result[k] = statistic(v[b])
+        
+    return result
+
+
 def qtcount(x, y, xmin, xmax, ymin, ymax, density=True):
     '''
     given rectangular output ranges for cells/leafs from QThist
     count the occurence rate of NEW data in these cells
+    
+    NOTE: simply wraps `binned_statistic_qt`, but kept here for compatibility
 
     Parameters
     ----------
@@ -114,18 +217,8 @@ def qtcount(x, y, xmin, xmax, ymin, ymax, density=True):
     Returns
     -------
     num : the array of number counts or densities per bin
+    
     '''
-
-    num = np.zeros_like(xmin)
-    for k in range(len(xmin)):
-        num[k] = np.sum((x >= xmin[k]) & (x < xmax[k]) &
-                        (y >= ymin[k]) & (y < ymax[k]))
-
-    if density:
-        # following example from np.histogram:
-        # result is the value of the probability *density* function at the bin,
-        # normalized such that the *integral* over the range is 1
-
-        num = num / ((ymax - ymin) * (xmax - xmin)) / num.sum()
+    num = binned_statistic_qt(x,y, statistic='count', edges=(xmin, xmax, ymin, ymax))
 
     return num
